6144c66b5c749eeae9eae60494c9d22c0b7a94d3
Bundit Nuntates <silkyland@gmail.com>
2023-01-31 22:36:51 +0700

    update

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ Plugin Name: WordPress Simple Bio Box
 Plugin URI: https://github.com/silkyland/wordpress-simbple-bio-box
 Description: Adds a simple bio box to your website
 Version: 1.0
-Author: Bundit Nunates
+Author: Bundit Nuntates
 Author URI: https://devded.com
 License: WTFPL
 */
 
 function simple_bio_box_shortcode()
 {
-    return '<div class="simple-bio-box">
+  return '<div class="simple-bio-box">
              <div class="simple-bio-box-left">
                ' . get_avatar(get_the_author_meta('ID'), 150) . '
              </div>
@@ -26,7 +26,7 @@ add_shortcode('simple_bio_box', 'simple_bio_box_shortcode');
 
 function simple_bio_box_styles()
 {
-    wp_enqueue_style('simple-bio-box-style', plugins_url('/css/style.css', __FILE__));
+  wp_enqueue_style('simple-bio-box-style', plugins_url('/css/style.css', __FILE__));
 }
 
 add_action('wp_enqueue_scripts', 'simple_bio_box_styles');
